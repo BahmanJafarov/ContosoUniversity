8d06b9d62a45bd057e3b4d902a145b60110b03e6
Bəhmən Cəfərov <30909255+Bahman-Jafarov@users.noreply.github.com>
2021-03-03 04:09:31 +0400

    .

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
---
+++ Models/SchoolViewModels/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class CourseViewModel
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
